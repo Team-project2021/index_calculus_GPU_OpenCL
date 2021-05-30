0beb2a89151067c920ead8e21cec240fa886df3f
matchupikchu <56771910+matchupikchu@users.noreply.github.com>
2021-05-30 21:26:06 +0200

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "Header.h"
 #include "Header1.h"
+#include "Header2.h"
+#include "gcd.cpp"
 
 int main()
 {
@@ -8,10 +10,21 @@ int main()
     unsigned int truths = 0;
     for (int i = 0; i < 100; i++)
     {
-        if (test_mult(A[i], B[i], C[i]) == true)
+        if (test_mult(mult_A[i], mult_B[i], mult_C[i]) == true)
             truths++;
     }
-    printf("%d", truths);
+    printf("%d\n", truths);
+
+    unsigned long long x, y;
+    //unsigned long long gcd = gcdExtended(gcd/*_a[2], gcd_b[2], &xd, &yd);
+    //printf("%llu, %llu, %llu", gcd, xd, yd);*/
+    unsigned int truths_gcd = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        if (test_gcdExtended(gcd_a[i], gcd_b[i],gcd_c[i]) == true)
+            truths_gcd++;
+    }
+    printf("%d\n", truths_gcd);
     return 0;
 
 }
---
+++ gcd.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
+
+unsigned long long gcdExtended(unsigned long long a, unsigned long long b, unsigned long long* x, unsigned long long* y) {
+    if (a == 0) {
+        *x = 0;
+        *y = 1;
+        return b;
+    }
+    unsigned long long x1, y1;
+    unsigned long long gcd = gcdExtended(b % a, a, &x1, &y1);
+    *x = y1 - (b / a) * x1;
+    *y = x1;
+    return gcd;
+}
+
+bool test_gcdExtended(unsigned long long a, unsigned long long b, unsigned long long c)
+{
+    unsigned long long x, y;
+    if (gcdExtended(a, b, &x, &y) == c)
+        return true;
+    else
+        return false;
+}
