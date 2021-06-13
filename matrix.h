d11b56f0bb4d1723fdd11964532a2023751b08b2
b-garbacz <45511879+b-garbacz@users.noreply.github.com>
2021-06-14 00:59:53 +0200

    14.06.2021

--- matrix.h
+++ matrix.h
@@ -2,7 +2,7 @@
 #include <vector>
 #include <stdexcept>
 #include <iostream>
-
+using namespace std;
 uint64_t gcdExtended(uint64_t a, uint64_t b, uint64_t* x, uint64_t* y)
 {
 	if (a == 0)
@@ -29,9 +29,10 @@ uint64_t gcd(uint64_t a, uint64_t b) {
 class Matrix
 {
 private:
-	std::vector<uint64_t> matrix;
 	const size_t cols;
+	std::vector<uint64_t> matrix;
 public:
+	
 
 	Matrix(size_t c): cols(c) {
 	}
@@ -88,5 +89,38 @@ public:
 	}
 	size_t size() {
 		return matrix.size();
+	}
+
+	vector<uint64_t> slicing(vector<uint64_t>& arr,
+		uint64_t X, uint64_t Y)
+	{
+
+		// Starting and Ending iterators
+		auto start = arr.begin() + X;
+		auto end = arr.begin() + Y + 1;
+
+		// To store the sliced vector
+		vector<uint64_t> result(Y - X + 1);
+
+		// Copy vector using copy function()
+		copy(start, end, result.begin());
+
+		// Return the final sliced vector
+		return result;
+	}
+
+	vector<uint64_t> get_row(uint64_t i)
+	{
+		vector<uint64_t> temp = vector<uint64_t>(this->matrix.begin() + (i * this->n_cols()), this->matrix.begin() + ((i + 1) * (this->n_cols())));
+		return temp;
+	}
+
+	void swap_rows(uint64_t i, uint64_t j) {
+
+		vector<uint64_t> temp = get_row(j);
+		for (int k = 0; k < this->n_cols(); k++) {
+			this->matrix[k + j * this->n_cols()] = this->operator()(i, k);
+			this->matrix[k + i * this->n_cols()] = temp[k];
+		}
 	}
 };
