5f86b3c3b538dd69cad708dddf08f47940992741
b-garbacz <45511879+b-garbacz@users.noreply.github.com>
2021-06-18 10:41:05 +0200

    18.06.2021
    Dodana faktoryzacja

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -6,15 +6,70 @@
 #include <CL/cl.h>
 #include <vector>
 #include "matrix.h"
+#include <cmath>
+//vector<uint64_t> faktoryzacja(vector<uint64_t>& arr, uint64_t N) {
+//
+//
+//
+//}
 
+
+bool is_integer(float k)
+{
+    if (k == (uint64_t)k) return true;
+    return false;
+}
+
+vector<uint64_t> factor(uint64_t N){
+    std::vector<uint64_t> factors;
+    std::vector<uint64_t> powers;
+    std::vector<uint64_t> score;
+    int  i = 2;
+    bool pom;
+    while(N > 1){
+        if(N%i == 0) {
+            N = N / i;
+            //std::cout << N << endl;
+            pom = true;
+            for (int j = 0; j < factors.size(); j++) {
+                if (factors[j] == i) {
+                    powers[j] = powers[j] + 1;
+                    pom = false;
+
+                    }
+           
+                }
+            if (pom == true) {
+                factors.push_back(i);
+                powers.push_back(1);
+                
+            }
+            i = i - 1;
+            
+        }
+        i = i + 1;
+        
+    }
+    for (int i = 0; i < factors.size(); i++) {
+        score.push_back(factors[i]);
+        score.push_back(powers[i]);
+    }
+    return score;
+}
+
+void print_vector(vector<uint64_t>& vec) {
+    for (int i = 0; i < vec.size(); i++) {
+        std::cout << vec[i] << " ";
+    }
+}
 int main()
-{   
-    Matrix m(5);
+{
+    /*Matrix m(5);
     m.add_row({ 1, 2, 3, 4, 5 });
     m.add_row({ 6, 7, 8, 9, 10});
     m.add_row({ 4, 44,6, 1, 88});
     m(2, 1) = 15;
-    m.print_matrix();
+    m.print_matrix();*/
     /*int res = m.find_inv(1, 30);
     std::cout << res << "\n";
     res = m.find_inv(1, 210);
@@ -46,11 +101,39 @@ int main()
     /*std::vector<cl_ulong> V({ 1,2,6,6,6,6,6,6,6 });
     vector_mult(V, 2);*/
 
-    m.swap_rows(0, 1);
+    /*m.swap_rows(0, 1);
 
-    m.print_matrix();
+    m.print_matrix();*/
 
+    std::vector<uint64_t> wynik;
+    wynik = factor(162);
+    print_vector(wynik);
+    std::cout << endl;
+
+    wynik = factor(625);
+    print_vector(wynik);
+    std::cout << endl;
+
+    wynik = factor(324324553456);
+    print_vector(wynik);
+    std::cout << endl;
+
+    wynik = factor(9);
+    print_vector(wynik);
+    std::cout << endl;
+
+    wynik = factor(5);
+    print_vector(wynik);
+    std::cout << endl;
+
+    wynik = factor(12);
+    print_vector(wynik);
+    std::cout << endl;
+
+    wynik = factor(42);
+    print_vector(wynik);
+    
+    
     return 0;
-
 }
 
